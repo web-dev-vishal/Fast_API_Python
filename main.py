f8b0e9564e309b3d63577dfbb488b9c45eaee26c
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-04 12:54:41 +0530

    first commit

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/{id}")
 def root(id: int):
     return {"message": f"Hello, FastAPI with ID {id}"}
+
+@app.post("/todo")
+def create_todo(item: dict):
+    return {"message": "Todo created", "item": item}
